7bf8792d9b055a8812ea871b57ab69cb1bdb5ec2
Anzil619 <anzilaz24@gmail.com>
2023-10-23 16:09:57 +0530

    added requirement text to configure gitignore

--- DecorConnect/settings.py
+++ DecorConnect/settings.py
@@ -172,12 +172,12 @@ CHANNEL_LAYERS = {
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'anzilaz24@gmail.com'
-EMAIL_HOST_PASSWORD = 'wpufdeujigqywrrd'
+EMAIL_HOST_USER =config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'anzilaz24@gmail.com'
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
 
